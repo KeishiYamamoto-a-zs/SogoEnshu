f6933f1688db96ef35989193212b8f885b6a5cfd
KeshiYamamoto.a-zs <k-yamamoto@a-zs.co.jp>
2023-06-26 16:44:11 +0900

    FirstCommit

---
+++ src/main/java/com/example/SogoEnshuOnSpringBoot1Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SogoEnshuOnSpringBoot1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SogoEnshuOnSpringBoot1Application.class, args);
+	}
+
+}
