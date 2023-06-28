03ddf7d164773817b2e73ee4b494eb5f894e6807
KeshiYamamoto.a-zs <k-yamamoto@a-zs.co.jp>
2023-06-28 10:34:19 +0900

    11番終了

--- src/main/java/com/example/Controller/InputFormController.java
+++ src/main/java/com/example/Controller/InputFormController.java
@@ -1,46 +1,50 @@
 package com.example.Controller;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.ItemList;
 import com.example.repository.ItemRepository;
 
 import lombok.extern.slf4j.Slf4j;
 
-
 @Slf4j
 @Controller
-public class VegetableController {
+public class InputFormController {
 	
 	@Autowired
 	private ItemRepository itemrepository;
 
-	@GetMapping("/Vegetable")
-	public String getVegetabel(Model model){
-		List<ItemList>vegetables = itemrepository.findAll() ;
-		model.addAttribute("items",vegetables);//キーitemsでリストvegetablesをTimeLeafに渡す
-		log.info(vegetables.toString());
+     
+	@GetMapping("/InputForm")
+	public String getInputForm() {
 		
-		//Vegetable.htmlに遷移
-		return "Vegetable";
+		return "InputForm";
+	}
+
+	@PostMapping("/InputForm")
+	public String postRequest(@RequestParam("text1")String str, Model model) {
+		ItemList item = new ItemList();
+	    item.setName(str);
+	    itemrepository.save(item);
 		
+		model.addAttribute("input", str);
+		
+				log.info(str);
+
+		return "redirect:/Vegetable";
 	}
 	
-	@PostMapping("/Vegetable")
+	@PostMapping("/ToList")
 	public String postRequest() {
 	
 		
-		return "InputForm";
+		return "redirect:/Vegetable";
 		
 	}
-		
-		
-		
+	
 }
-
-
