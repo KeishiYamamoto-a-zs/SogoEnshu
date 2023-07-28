9d297bda1e15850dff93b51b01483cf94c1d5cc5
KeshiYamamoto.a-zs <k-yamamoto@a-zs.co.jp>
2023-07-28 14:04:35 +0900

    Controllerクラスのリファクタリングを実装

--- src/main/java/com/example/Controller/DetailVegetableController.java
+++ src/main/java/com/example/Controller/DetailVegetableController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -41,7 +42,11 @@ public class DetailVegetableController {
       }
       
       @PostMapping("/updateList")
-  	public String updateList(@ModelAttribute InputForm inputForm,BindingResult bindingResult) {
+  	public String updateList(@ModelAttribute @Validated InputForm inputForm,BindingResult bindingResult,Model model) {
+    	  
+    	  if(bindingResult.hasErrors()) {
+  	    	return getDetailVegetable(inputForm.getId(),model,inputForm);	    
+  		}	
     	  		  
   		String cutString = changeStringServiceImpl.cut(inputForm.getItems());
   		
